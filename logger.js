91e8942a75a1b1929d1e793777df3a58a7fd5459
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-01-20 19:52:21 +0000

    Bot now creates and adds to physical logs

--- logger.js
+++ logger.js
@@ -1,4 +1,8 @@
+var fs = require('fs');
+
 const sourceLength = 12;
+
+var currentLog = null;
 
 exports.logCommand = function (message) {
     var name = message.member.displayName;
@@ -14,7 +18,41 @@ exports.error = function (err, source) {
     logMessage('ERROR: ' + err, source);
 }
 
+function createNewLog() {
+    
+    if (currentLog != null) {
+        logMessage("Attempted to create new log when one already existed for the current session.");
+        return;
+    }
+
+    var padNum = (str, size) => padStart(str, '0', size);
+    var now = new Date();
+
+    var date = "";
+    date += now.getFullYear();
+    date += padNum((now.getMonth() + 1), 2);
+    date += padNum(now.getDate(), 2);
+    date += padNum(now.getHours(), 2);
+    date += padNum(now.getMinutes(), 2);
+    date += padNum(now.getSeconds(), 2);
+
+    currentLog = fs.createWriteStream("logs/" + date + ".txt");
+}
+
+function addToLog(addition) {
+    if (currentLog == null) {
+        logMessage("Attempted to add to a log when one didn't exist.");
+        return;
+    }
+
+    currentLog.write(addition + "\r\n");
+}
+
 function logMessage(message, source) {
+    
+    if (currentLog == null)
+        createNewLog()
+
     var now = new Date();
     var padNum = (str, size) => padStart(str, '0', size);
     
@@ -34,6 +72,8 @@ function logMessage(message, source) {
     dateStr += padNum(now.getSeconds(), 2) + ']\t';
 
     console.log(dateStr + source + message);
+
+    addToLog(dateStr + source + message);
 }
 
 function padStart(str, padChar, targetLength) {
