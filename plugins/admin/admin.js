c248bf4591641ae53961bd68ee559a104646c53d
ClinkMakesGames <32272073+ClinkMakesGames@users.noreply.github.com>
2018-02-03 07:39:11 +0000

    Added ability to purge a user's last x messages in a channel

--- plugins/admin/admin.js
+++ plugins/admin/admin.js
@@ -32,7 +32,7 @@ exports.init = function (client, config, package, logger) {
     if (self.config.muteRoleName) {
         self.muteRole = self.config.muteRoleName;
         self.logger.log('set muted role to: ' + self.muteRole, 'admin');
-    } 
+    }
 }
 
 exports['say'] = {
@@ -68,7 +68,7 @@ exports['reset'] = {
 }
 
 exports['purge'] = {
-    usage: 'purge <number> | Bulk delete most recent messages in a channel',
+    usage: 'purge <number> (@user) | Delete the last <number> messages in a channel (by @user)',
     process: function (message, args) {
         var number = parseInt(args[0]);
         if (!number)
@@ -77,6 +77,46 @@ exports['purge'] = {
         if (number > 99 || number < 0) {
             message.reply("there is a maxiumum value of 99 messages and a minimum value of 0.")
                 .then((msg) => { msg.delete(5000) })
+                .catch(self.logger.error);
+            return;
+        }
+
+        var target = message.mentions.members
+            .map((member, _) => member)[0];
+
+        if (target != null) {
+            var sorted = [];
+
+            message.channel.fetchMessages({ limit: 100 })
+                .then(messages => {
+                    messages = messages.array();
+
+                    for (var i = 0; i < messages.length; i++) {
+                        if (messages[i].member == target && sorted.length < number) {
+                            sorted.push(messages[i]);
+                        }
+                    }
+
+                    message.channel.send(`Deleting ${sorted.length} messages by ${target.user.tag} in this channel.`)
+                        .then(m => {
+                            var delay = 2000;
+                            ((t) => {
+                                var start = new Date().getTime();
+                                for (var i = 0; i < 1e7; i++) {
+                                    if ((new Date().getTime() - start) > t)
+                                        break;
+                                }
+                            })(delay);
+
+                            m.delete()
+                                .catch(self.logger.error);
+                            for (var x = 0; x < sorted.length; x++) {
+                                sorted[x].delete()
+                                    .catch(self.logger.error);
+                            }
+                        })
+                        .catch(self.logger.error);
+                })
                 .catch(self.logger.error);
             return;
         }
@@ -96,7 +136,7 @@ exports['purge'] = {
                     .catch(self.logger.error);
             })
             .catch(self.logger.error);
-    }
+        }
 }
 
 exports['ping'] = {
@@ -123,7 +163,7 @@ exports['tempmute'] = {
             }
         }
 
-        if (args.length != 2) 
+        if (args.length != 2)
             return;
 
 
@@ -131,7 +171,7 @@ exports['tempmute'] = {
             return;
 
         var seconds = args[1];
-        
+
         var muteRole = message.guild.roles.find('name', self.muteRole);
         if (!muteRole) {
             self.logger.log('Unable to mute: couldn\'t find role ' + self.muteRole, 'admin');
