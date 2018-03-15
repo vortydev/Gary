52d4ddd094db1e50645e30d1f80923a88db5c643
Cat Flynn <mewtwo97@hotmail.com>
2018-03-15 18:01:40 +0000

    Fix poorly implemented nested loop

--- plugins/roles/roles.js
+++ plugins/roles/roles.js
@@ -67,10 +67,11 @@ exports['role'] = {
 
         if (!message.member.roles.has(serverRole.id)) {
             if (self.config.mutuallyExclusiveRoles) {
+                
                 for (var i = 0; i < self.config.assignableRoles.length; i++) {
-                    for (var i = 0; i < self.config.roles.length; i++) {
-                        var serverR = message.guild.roles.find("name", self.config.roles[i].name);
-                        if (message.member.roles.has(serverR.id) && serverR != serverRole) {
+                    for (var j = 0; j < self.config.roles.length; j++) {
+                        var otherServerRole = message.guild.roles.find("name", self.config.roles[i].name);
+                        if (message.member.roles.has(otherServerRole.id) && otherServerRole != serverRole) {
                             message.reply("you already have a role!")
                                 .then(m => m.delete(5000));
                             return;
