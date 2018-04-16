08dd5802629118e041315b00501cc51ada322704
Cat Flynn <mewtwo97@hotmail.com>
2018-04-16 23:34:07 +0100

    Use messager in cats plugin

--- plugins/cats/cats.js
+++ plugins/cats/cats.js
@@ -7,11 +7,13 @@ var catsconfig = require('./catsconfig.json');
 self.client = null;
 self.config = null;
 self.logger = null;
+self.messager = null;
 
 exports.init = function (context) {
     self.client = context.client;
     self.config = context.config;
     self.logger = context.logger;
+    self.messager = context.messager;
 
     if (catsconfig.postToChannelID == '' || !catsconfig.postEvery) {
         self.logger.log('catsconfig.json needs to be filled in for the cats module to work.', 'cats');
@@ -31,23 +33,29 @@ function sendCat() {
 
     var caturl = 'http://aws.random.cat/meow.php';
 
-    request({
-        url: caturl,
-        json: false
-        }, function (error, response, body) {
-            cat = JSON.parse(body);
-            if (error) {
-                self.logger.log(cat.error, 'cats');
+    request(
+        {
+            url: caturl,
+            json: false
+        }, 
+        function (error, response, body) {
+
+            var cat = null;
+            try {
+                cat = JSON.parse(body);
+            } catch (e) {
+                self.logger.log(e, 'cats');
                 return;
             }
+            
             var embed = new Discord.RichEmbed()
                 .setColor(self.config.embedCol)
                 .setTitle('A random cat')
                 .setDescription('Next cat in ' + catsconfig.postEvery + ' seconds')
                 .setImage(cat.file)
                 .setTimestamp();
-            channel.send({embed})
-              .catch(self.logger.error);
+            
+            self.messager.send(channel, { embed }, false);
         }
     );
 }
