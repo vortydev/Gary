ba0d34bc88ae99b742ce4848eb5eecadcb0a2ef2
TheV0rtex <etienne.dmenard@gmail.com>
2018-06-17 12:12:28 -0400

    Fixed syntax error in "tickets.js"

--- plugins/tickets/tickets.js
+++ plugins/tickets/tickets.js
@@ -5,7 +5,16 @@ var Discord = require("discord.js");
 
 const ticketsPath = './plugins/tickets/tickets.json';
 
-let tickets = JSON.parse(fs.readFileSync(ticketsPath, "utf8")); // ticket system
+if (!fs.existsSync(ticketsPath)) {
+    fs.writeFile(
+        './plugins/tickets/tickets.json',
+        '{}',
+        function (ticketsLog) {
+        console.log('"tickets.json" was created.');
+    });
+}
+
+var tickets = JSON.parse(fs.readFileSync("./plugins/tickets/tickets.json", "utf8")); // ticket system
 
 self.config = null;
 self.logger = null;
@@ -24,27 +33,27 @@ exports.init = function (client, config, _, logger) {
 exports['ticket new'] = {
     usage: 'Opens up a new ticket.',
     process: function (message, args) {
-        if (!message.guild.roles.exists("name", "perms")) { // If role not found
-            message.reply("this server doesn't have a \`perms` role, so the ticket won't be opened.\n"+
+        if (!message.guild.roles.exists("name", config.supportRoleName)) { // If role not found
+            message.reply("this server doesn't have a `"+ config.supportRoleName +"` role, so the ticket won't be opened.\n"+
                           "If you are an administrator, make a role with that name exactly and give it to users that will answer the tickets.").catch(self.logger.error);
             return;
         }
 
         if (!tickets[client.user.id]) tickets[client.user.id] = {tickets:0}; // sets values to 0
 
-        let ticketData = tickets[client.user.id];
+        var ticketData = tickets[client.user.id];
         if (ticketData.tickets < 10000) {
         // adds 1 to the ticket count
-        ticketData.tickets++.catch(self.logger.error);
+        ticketData.tickets++;
         };
 
         fs.writeFile(ticketsPath, JSON.stringify(tickets), (err) => {
             if (err) console.error(err);
         });
 
-        message.guild.createChannel("ticket-"+ticketData.tickets, "text").then(chnl => {
-            let support = message.guild.roles.find("name", "perms");
-            let everyone = message.guild.roles.find("name", "@everyone");
+        message.guild.createChannel("ticket-"+ ticketData.tickets, "text").then(chnl => {
+            var support = message.guild.roles.find("name", config.supportRoleName);
+            var everyone = message.guild.roles.find("name", "@everyone");
             chnl.overwritePermissions(message.author, {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
@@ -59,7 +68,7 @@ exports['ticket new'] = {
                 READ_MESSAGES: false
             });
 
-            message.reply("your ticket has been created: `"+chnl.name+"`.");
+            message.reply("your ticket has been created: `"+ chnl.name +"`.");
             chnl.send(message.author+", please explain the reason for this ticket with as much precision and details as you can. Our **"+support.name+"** will be here to help you as soon as possible.").catch(self.logger.error);
 
         }).catch(self.logger.error);
@@ -71,7 +80,7 @@ exports['ticket new'] = {
 exports['ticket close'] = {
     usage: 'Closes a ticket that has been resolved or been opened by accident.',
     process: function (message, args) {
-        if (!message.guild.roles.exists("name", "Support Team")) { // If role not found
+        if (!message.guild.roles.exists("name", config.supportRoleName)) { // If role not found
             message.reply("this server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\n"+
                           "If you are an administrator, make a role with that name exactly and give it to users that will answer the tickets.").catch(self.logger.error);
             return;
@@ -85,7 +94,7 @@ exports['ticket close'] = {
         message.channel.send("Are you sure? Once confirmed, you cannot reverse this action!\n"+
                               "To confirm, please type \`"+config.prefix+"confirm\`. This will cancel in 20 seconds.")
         .then((msg) => {
-            message.channel.awaitMessages(response => response.content === config.prefix+"confirm", { // $confirm
+            message.channel.awaitMessages(response => response.content === config.prefix +"confirm", { // $confirm
                 max: 1,
                 time: 20000, // wait 20 secounds
                 errors: ["time"],
@@ -107,14 +116,15 @@ exports['ticket close'] = {
 exports['ticket count'] = {
     usage: 'Sends the user the number of opened tickets.',
     process: function (message, args) {
-        if (!message.guild.roles.exists("name", "Support Team")) { // If role not found
-            message.reply("this server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\n"+
+        if (!message.guild.roles.exists("name", config.supportRoleName)) { // If role not found
+            message.reply("this server doesn't have a `"+ config.supportRoleName +"` role made, so the ticket won't be opened.\n"+
                           "If you are an administrator, make a role with that name exactly and give it to users that will answer the tickets.").catch(self.logger.error);
             return;
         }
+
         if (!tickets[client.user.id]) tickets[client.user.id] = {tickets:0}; // sets values to 0
 
-        let ticketData = tickets[client.user.id];
+        var ticketData = tickets[client.user.id];
 
         fs.writeFile(ticketsPath, JSON.stringify(tickets), (err) => {
             if (err) console.error(err)
